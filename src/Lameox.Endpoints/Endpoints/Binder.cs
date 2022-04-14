3c50e4b501789e7b5140ca9988175e4e1ec21a3b
lameox <finkenzellerjan@gmail.com>
2022-04-14 18:08:18 +0200

    factor binding into its own file

--- src/Lameox.Endpoints/Endpoints/Binder.cs
+++ src/Lameox.Endpoints/Endpoints/Binder.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Http;
 
 namespace Lameox.Endpoints
 {
-    internal static partial class Binder<TRequest>
+    internal static partial class Binder<TRequest> where TRequest : notnull
     {
         private static readonly ImmutableDictionary<string, PropertySetter> _propertySetters;
 
@@ -17,86 +17,6 @@ namespace Lameox.Endpoints
             _propertySetters = typeof(TRequest).GetProperties().ToImmutableDictionary(p => p.Name, p => PropertySetter.Create(p));
         }
 
-        public static ValueTask<ImmutableArray<BindingFailure>> BindRequestValuesAsync(ref TRequest request, HttpContext requestContext)
-        {
-            var failures = new FailureCollection();
 
-            BindFormValues(ref request, requestContext, ref failures);
-            BindRouteValues(ref request, requestContext, ref failures);
-            BindQueryParameters(ref request, requestContext, ref failures);
-
-            return ValueTask.FromResult(failures.ToImmutable());
-        }
-
-        private static void BindFormValues(ref TRequest request, HttpContext requestContext, ref FailureCollection failures)
-        {
-            if (!requestContext.Request.HasFormContentType)
-            {
-                return;
-            }
-
-            foreach (var formField in requestContext.Request.Form)
-            {
-                Bind(ref request, formField.Key, formField.Value[0], ref failures);
-            }
-
-            foreach (var formFile in requestContext.Request.Form.Files)
-            {
-                if (_propertySetters.TryGetValue(formFile.Name, out var propertySetter))
-                {
-                    if (propertySetter.CanSetValueDirectly<IFormFile>() || !propertySetter.TryParseAndSet(ref request, formFile))
-                    {
-                        failures.Add(new(propertySetter.PropertyType, formFile.Name, null, $"Form files can only be bound to Properties of type {nameof(IFormFile)}."));
-                    }
-                }
-            }
-
-            return;
-        }
-
-        private static void BindRouteValues(ref TRequest request, HttpContext requestContext, ref FailureCollection failures)
-        {
-            if (!requestContext.Request.RouteValues.Any())
-            {
-                return;
-            }
-
-            foreach (var routeValue in requestContext.Request.RouteValues)
-            {
-                Bind(ref request, routeValue.Key, routeValue.Value, ref failures);
-            }
-
-            return;
-        }
-
-        private static void BindQueryParameters(ref TRequest request, HttpContext requestContext, ref FailureCollection failures)
-        {
-            if (!requestContext.Request.Query.Any())
-            {
-                return;
-            }
-
-            foreach (var queryParameter in requestContext.Request.Query)
-            {
-                Bind(ref request, queryParameter.Key, queryParameter.Value[0], ref failures);
-            }
-
-            return;
-        }
-
-        private static void Bind(ref TRequest request, string key, object? value, ref FailureCollection failures)
-        {
-            if (!_propertySetters.TryGetValue(key, out var propertySetter))
-            {
-                return;
-            }
-
-            if (!propertySetter.TryParseAndSet(ref request, value))
-            {
-                failures.Add(new BindingFailure(propertySetter.PropertyType, key, value));
-            }
-
-            return;
-        }
     }
 }
