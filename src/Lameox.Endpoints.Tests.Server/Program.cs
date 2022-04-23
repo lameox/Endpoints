a30c73f1a6482099f182923181e27142c74068c6
lameox <finkenzellerjan@gmail.com>
2022-04-23 14:13:35 +0200

    set up test server with auth

--- src/Lameox.Endpoints.Tests.Server/Program.cs
+++ src/Lameox.Endpoints.Tests.Server/Program.cs
@@ -1,9 +1,31 @@
 ﻿using Lameox.Endpoints;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSimpleEndpoints();
 
+var validationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+{
+    IssuerSigningKey = new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("TestKeyTestKeyTestKeyTestKeyTestKey")),
+    ValidateIssuer = false,
+    ValidateAudience = false,
+    ValidateLifetime = false,
+    ValidateIssuerSigningKey = false,
+};
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = validationParameters;
+});
+
 var app = builder.Build();
+app.UseAuthentication();
 app.UseSimpleEndpoints();
 
 app.MapSimpleEndpoints();
