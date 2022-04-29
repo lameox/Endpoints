4441d5753af4b15824b93cfe123ff35369b6dbf1
lameox <finkenzellerjan@gmail.com>
2022-04-29 13:41:20 +0200

    added helper class for pooling of immutable array builders

---
+++ src/Lameox.Endpoints/ImmutableArrayBuilderPool.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lameox.Endpoints
+{
+    public interface IArrayBuilderOwner<T> : IDisposable
+    {
+        ImmutableArray<T>.Builder Builder { get; }
+    }
+
+    internal class ImmutableArrayBuilderPool<T>
+    {
+        private readonly struct Reservation : IArrayBuilderOwner<T>
+        {
+            public ImmutableArray<T>.Builder Builder { get; }
+
+            public Reservation(ImmutableArray<T>.Builder builder)
+            {
+                Builder = builder;
+            }
+
+            public void Dispose()
+            {
+                ReturnInstance(Builder);
+            }
+        }
+
+        private static readonly ObjectPool<ImmutableArray<T>.Builder> _pool =
+            new ObjectPool<ImmutableArray<T>.Builder>(() => ImmutableArray.CreateBuilder<T>());
+
+        public static ImmutableArray<T>.Builder GetInstance()
+        {
+            var result = _pool.Allocate();
+            return result;
+        }
+
+        public static void ReturnInstance(ImmutableArray<T>.Builder instance)
+        {
+            if (instance.Capacity > 128)
+            {
+                return;
+            }
+
+            instance.Clear();
+            _pool.Free(instance);
+        }
+
+        public static IArrayBuilderOwner<T> Get()
+        {
+            var instance = GetInstance();
+            return new Reservation(instance);
+        }
+    }
+}
